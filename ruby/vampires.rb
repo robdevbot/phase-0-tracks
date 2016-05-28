3493a8925bb27135a5d9d0d188b6f9f761dcda39
robzd1 <robzd1@gmail.com>
2016-05-28 21:13:37 +0000

    Add variable type processing and basic results logic

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -8,9 +8,36 @@ puts "What year were you born?"
 year_born = gets.chomp
 
 puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-garlicbread = gets.chomp
+gb_response = gets.chomp
 
 puts "Would you like to enroll in the company’s health insurance?"
-insurance = gets.chomp
+ins_response = gets.chomp
+
+calculated_age = 2016 - year_born.to_i
 
 
+garlicbread = false
+insurance = false
+
+if (gb_response == "yes")
+    garlicbread = true
+end
+
+if (ins_response == "yes")
+    insurance = true
+end
+
+
+puts "Name: #{name}"
+puts "Age: #{age}"
+puts "Year born: #{year_born}"
+puts "Calculated age: #{calculated_age}"
+puts "Garlic Bread?: #{garlicbread}"
+puts "Insurance?: #{insurance}"
+
+
+if (age.to_i == calculated_age) && (garlicbread || insurance)
+    puts "Probably a vampire"
+else
+    puts "Results inconclusive."
+end
