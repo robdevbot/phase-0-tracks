bafa5a81eb77a46ac86848de2811e94f2b07852e
robzd1 <robzd1@gmail.com>
2016-06-05 13:48:39 +0000

    Add prompts, inputs, and hash creation

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -7,3 +7,38 @@
 # if the input isn't 'done', prompt the user for a new value
 # convert data-name to a symbol and update that entry in the hash with the new value
 # Print the hash to the screen
+
+
+puts "Please enter your name"
+name = gets.chomp
+
+puts "Please enter your age"
+age = gets.chomp.to_i
+
+puts "Please enter your number_of_children"
+number_of_children = gets.chomp.to_i
+
+puts "Please enter your decor_theme"
+decor_theme = gets.chomp
+
+puts "Please enter your likes_lime"
+likes_lime = gets.chomp.to_b
+
+puts "Please enter your needs_sandproofing"
+needs_sandproofing = gets.chomp.to_b
+
+puts "Please enter your vintage_level"
+vintage_level = gets.chomp
+
+
+customer_data = {
+    name: name,
+    age: age,
+    number_of_children: number_of_children,
+    decor_theme: decor_theme,
+    likes_lime: likes_lime,
+    needs_sandproofing: needs_sandproofing,
+    vintage_level: vintage_level
+}
+
+puts customer_data
