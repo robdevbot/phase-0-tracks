5522fec157d1fa2554910c3b904ef3d1d173ece3
robzd1 <robzd1@gmail.com>
2016-06-05 13:42:01 +0000

    Add pseudocode for customer preference program

---
+++ ruby/hashes.rb
@@ -0,0 +1,9 @@
+# Prompt the user for info: name, age, number of children, decor theme, likes_lime, needs_sandproofing, vintage_level
+# Take that info in
+# Create a hash with all of that info
+# Print the hash to the screen
+# Prompt the user asking if they want to update any values
+# take an input of the data-name to be changed
+# if the input isn't 'done', prompt the user for a new value
+# convert data-name to a symbol and update that entry in the hash with the new value
+# Print the hash to the screen
