7366b6afd5573619e1deb0139ac37aa3c8070a4a
robzd1 <robzd1@gmail.com>
2016-06-07 17:18:58 +0000

    Add puppy class and driver code from pairing with Kevin

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,32 @@
 class Puppy
-
   def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
+    puts "I brought back the #{toy}"
   end
 
+  def speak(x)
+    x.times{puts "Woof"}
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(x)
+    p x*7
+  end
+
+  def heel
+    puts "I like walking next to my human"
+  end
+
+  def initialize
+    puts "Initializing new puppy instance..."
+  end
 end
 
+rex = Puppy.new
+rex.fetch("ball")
+rex.speak(5)
+rex.roll_over
+rex.dog_years(7)
+rex.heel
