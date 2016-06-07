2cff399dde85ebd8de1ec9959b88cf89ff1272c7
robzd1 <robzd1@gmail.com>
2016-06-07 17:23:04 +0000

    Add basic student class with 3 methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,32 +1,49 @@
-class Puppy
-  def fetch(toy)
-    puts "I brought back the #{toy}"
-  end
+# class Puppy
+#   def fetch(toy)
+#     puts "I brought back the #{toy}"
+#   end
 
-  def speak(x)
-    x.times{puts "Woof"}
-  end
+#   def speak(x)
+#     x.times{puts "Woof"}
+#   end
 
-  def roll_over
-    puts "*rolls over*"
-  end
+#   def roll_over
+#     puts "*rolls over*"
+#   end
 
-  def dog_years(x)
-    p x*7
-  end
+#   def dog_years(x)
+#     p x*7
+#   end
 
-  def heel
-    puts "I like walking next to my human"
-  end
+#   def heel
+#     puts "I like walking next to my human"
+#   end
 
+#   def initialize
+#     puts "Initializing new puppy instance..."
+#   end
+# end
+
+# rex = Puppy.new
+# rex.fetch("ball")
+# rex.speak(5)
+# rex.roll_over
+# rex.dog_years(7)
+# rex.heel
+
+class Student
   def initialize
-    puts "Initializing new puppy instance..."
+    puts "Creating a student."
   end
+
+  def study(hours)
+    hours.times { puts "Studied for an hour." }
+  end
+
+  def party(drinks)
+    puts "Partied hard. Drank #{drinks} drinks."
+  end
+  
 end
 
-rex = Puppy.new
-rex.fetch("ball")
-rex.speak(5)
-rex.roll_over
-rex.dog_years(7)
-rex.heel
+
