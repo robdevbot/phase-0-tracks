ce0a940ec7c6f1f4c009ab8a1512d1194062763b
robzd1 <robzd1@gmail.com>
2016-06-07 17:51:57 +0000

    Add pseudoclass 'puppy' in comments

---
+++ ruby/puppy.rb
@@ -0,0 +1,20 @@
+# Puppy Class
+# -------------------------
+# Name
+# ------------
+# Rover
+
+# Characteristics
+# ------------
+# Fur: Brown
+# Ear style: floppy
+# Bark: "Arf"
+# Number of legs: 3
+# obedient: true
+
+# Behavior
+# ------------
+# Sits
+# Speaks
+# Shakes Hands
+# Fetches
