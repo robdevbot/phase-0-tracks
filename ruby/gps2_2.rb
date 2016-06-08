111eeed13e1085b1f74df760de64bdb8c2822c17
robzd1 <robzd1@gmail.com>
2016-06-08 00:09:30 +0000

    Add remove and update methods

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -20,30 +20,50 @@ puts groc_list
 
 
 
-def add_item(list, item, quantity)
-    list[item] = quantity 
-    puts groc_list
-end
-
-add_item(groc_list, "koolaid", 1)
-
-
-
 # Method to add an item to a list
 # input: the list, item name and optional quantity
 # steps: list[koolaid] => 1
 # output: the updated hash
 
-# groc_list.remove(koolaid)
+def add_item(list, item, quantity)
+    list[item] = quantity 
+    puts list
+end
+
+add_item(groc_list, "koolaid", 1)
+
+
+# remove(list, koolaid)
 # Method to remove an item from the list
 # input: the list, item name
 # steps: Hash.delete(item) 
 # output: the updated hash
 
+
+def remove_item(list, item)
+    list.delete_if {|key, value| key == item }
+    puts list
+end
+
+
+remove_item(groc_list, "koolaid")
+
+
 # Method to update the quantity of an item
 # input: the list, item name, new quantity
 # steps: select by key, reassign value
 # output: the updated hash
+
+def update_quantity(list, item, quantity)
+    list[item] = quantity
+    puts list
+end
+
+update_quantity(groc_list, "bananas", 10)
+
+
+
+
 
 # Method to print a list and make it look pretty
 # input: the list
