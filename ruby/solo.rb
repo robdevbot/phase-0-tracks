6a8cc3d4a33ee362edd4d487183671273031621f
robzd1 <robzd1@gmail.com>
2016-06-09 04:31:58 +0000

    Pseudocode for moped class

---
+++ ruby/solo.rb
@@ -0,0 +1,6 @@
+# Class - moped
+# has attributes, make(string), model(string), year(integer), and runs(boolean)
+# method intialize - sets variables, including runs with default value of true
+# method tune_engine - prints a fun message, makes sure runs is set to true
+# method tune_brakes - just prints a fun message
+# method add_gas - prints a message with an integer variable taken and returned for quantity
