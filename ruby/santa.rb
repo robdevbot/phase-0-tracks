ac21bf68f53d0e96a19b926743683a8459ec5f9f
robzd1 <robzd1@gmail.com>
2016-06-09 04:02:52 +0000

    Add methods change_gender, get_mad_at, celebrate_birthday, age, ethnicity

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -16,16 +16,48 @@ class Santa
     def eat_milk_and_cookies(cookie_type)
         puts "That was a good #{cookie_type}!"
     end
-   
+    
+    def celebrate_birthday
+        age += 1
+    end
+    
+    def get_mad_at(reindeer)
+        if @reindeer_ranking.include?(reindeer)
+            @reindeer_ranking.delete(reindeer)
+            @reindeer_ranking << reindeer
+        else
+            puts "Error: not a valid reindeer!"
+        end
+    end
+    
+    def change_gender(new_gender)
+        @gender = new_gender
+    end
+    
+    def age
+        @age
+    end
+    
+    def ethnicity
+        @ethnicity
+    end
+    
 end
 
-santas = []
-santas << Santa.new("agender", "black")
-santas << Santa.new("female", "Latino")
-santas << Santa.new("bigender", "white")
-santas << Santa.new("male", "Japanese")
-santas << Santa.new("female", "prefer not to say")
-santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
-santas << Santa.new("N/A", "N/A")
+# santas = []
+# santas << Santa.new("agender", "black")
+# santas << Santa.new("female", "Latino")
+# santas << Santa.new("bigender", "white")
+# santas << Santa.new("male", "Japanese")
+# santas << Santa.new("female", "prefer not to say")
+# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+# santas << Santa.new("N/A", "N/A")
 
-puts santas
+# puts santas
+
+fred_claus = Santa.new("male", "black")
+
+fred_claus.get_mad_at("Dancer")
+
+fred_claus.change_gender("female")
+
