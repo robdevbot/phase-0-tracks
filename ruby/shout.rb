2de86ee2b240160e1b0ce19fa97ca5bc48581e4c
robzd1 <robzd1@gmail.com>
2016-06-11 01:15:07 +0000

    Add some simple yelling methods

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,15 @@
 module Shout
+    def self.yell_angrily(words)
+        words + "!!!" + " :("
+    end
     
+    def self.yell_happily(words)
+        words.upcase + "!!!" + " :D"
+    end
 end
+
+
+puts Shout.singleton_methods
+
+puts Shout.yell_angrily("Hey!")
+puts Shout.yell_happily("Hey!")
