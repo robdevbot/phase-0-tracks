91a17e4545aa28c20cd819ea4c9d559abbf43a68
robzd1 <robzd1@gmail.com>
2016-06-11 01:27:05 +0000

    Change Shout to a mixin class and add driver code to test across different class instances

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,15 +1,44 @@
+# module Shout
+#     def self.yell_angrily(words)
+#         words + "!!!" + " :("
+#     end
+    
+#     def self.yell_happily(words)
+#         words.upcase + "!!!" + " :D"
+#     end
+# end
+
+# puts Shout.singleton_methods
+# puts Shout.yell_angrily("Hey!")
+# puts Shout.yell_happily("Hey!")
 module Shout
-    def self.yell_angrily(words)
+    def yell_angrily(words)
         words + "!!!" + " :("
     end
     
-    def self.yell_happily(words)
+    def yell_happily(words)
         words.upcase + "!!!" + " :D"
     end
 end
 
 
-puts Shout.singleton_methods
+class Politician
+    include Shout
+end
 
-puts Shout.yell_angrily("Hey!")
-puts Shout.yell_happily("Hey!")
+class Hero
+    include Shout
+end
+
+
+trump = Politician.new
+puts trump.yell_happily("A huge wall!")
+puts trump.yell_angrily("My hands are perfectly normal")
+
+
+bruce_campbell = Hero.new
+
+puts bruce_campbell.yell_angrily("Oooh that's gotta hurt")
+puts bruce_campbell.yell_happily("Hail to the king, baby")
+
+
