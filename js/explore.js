7f45a1b4ead5e6a4556cf99943a3c3265d6baeb1
robzd1 <robzd1@gmail.com>
2016-06-15 00:32:02 +0000

    Code out the solution to string reversal

--- js/explore.js
+++ js/explore.js
@@ -1,4 +1,15 @@
 // reverse string function
-// iterate backwards through a string, starting with the index at it's length and going to 0
+// iterate backwards through a string, starting with the index at it's length *minus one* and going to 0
 // for each character, add it to a new string
 // return the new string
+
+var inputString = "hello";
+var newString = "";
+
+for (i = inputString.length - 1; i >= 0; i--) {
+    // console.log("i is equal to " + i);
+    // console.log("character at index i is " + inputString[i]);
+    newString += inputString[i];
+}
+
+console.log(newString);
