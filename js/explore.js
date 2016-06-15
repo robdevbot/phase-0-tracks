8a575dc175c1787b3b0712f23eccfb9a7b2983d5
robzd1 <robzd1@gmail.com>
2016-06-15 00:26:55 +0000

    Add pseudocode for reverse string function

---
+++ js/explore.js
@@ -0,0 +1,4 @@
+// reverse string function
+// iterate backwards through a string, starting with the index at it's length and going to 0
+// for each character, add it to a new string
+// return the new string
