05e56eb5d3257408e924ab8b66f49a801db3c30c
robzd1 <robzd1@gmail.com>
2016-06-18 16:14:20 +0000

    Answer questions for Releases 1-3

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -1,11 +1,39 @@
 # Virus Predictor
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself
 # We spent [#] hours on this challenge.
 
+# Release 1
+# This program calculates some theoretical statistics based on some figures for the area
+# It defines a class, VirusPredictor, which can be constructed for any given state with a given population and population density
+# The class methods provide the calculations and outputs
+# In the provided examples, the data inputs are pulled from a hash of the 50 states, stored in the external file
+
+# Release 2
 # EXPLANATION OF require_relative
-#
-#
+# The require_relative statement in this program allows this file to use the content of the external file, state_data.rb.
+# By referencing the file name, the ruby compiler simply treats the two files as one combined file with all of the content of each.
+# require_relative accepts a path that is relative to the file calling it. In this case, state_data.rb is in the same directory as this file,
+# so require_relative 'state_data' is sufficient to reference it. This is notably cleaner than calling the entire path.
+
+# Release 3
+# The STATE_DATA hash in state_data.rb contains 50 key-value pairs, one for each state.
+# The key of each pair is a string with the name of the state
+# The value of each pair is another hash
+# Each of those hashes contains two key-value pairs
+# The keys are the symbols :population_density and :population.
+# The values are corresponding integer numbers.
+# The main hash, STATE_DATA, has its key-value pairs declared using the rocket syntax, like this - Key => Value
+# The sub-hashes in each value are defined using the colon syntax, like this - key: value
+# Rocket syntax is good because it allows keys of any object type, including the useful string keys used here.
+# Colon syntax is cleaner, but it requires the use of symbols as keys.
+# I would guess that the phrase "Hashy hash" refers to a hash containing more hashes.
+# STATE_DATA is a hash, and its scope is global, because it is declared via the require_relative keyword in my_solution.rb, outside of any methods or classes
+
+
+
+
+
 require_relative 'state_data'
 
 class VirusPredictor
@@ -15,6 +43,8 @@ class VirusPredictor
     @population = population
     @population_density = population_density
   end
+
+
 
   def virus_effects
     predicted_deaths(@population_density, @population, @state)
