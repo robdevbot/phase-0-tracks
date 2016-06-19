6dd31cc861842e2602b7d82ac239e92c652458e1
robzd1 <robzd1@gmail.com>
2016-06-19 20:04:28 +0000

    Add basic, non-working implementation for objects_equal function

--- js/algos.js
+++ js/algos.js
@@ -11,16 +11,29 @@ function longest(array) {
         }
     
     }
-    
-    return array[longest_index]
-
+    return array[longest_index];
 }
+
+function objects_equal(object1, object2) {
+    
+    for (i=0; i<object1.length; i++) {
+        console.log(object1[i]);
+    }
+    
+}
+
+
+
 
 
 var superheroes = ["Hulk", "Thor", "Iron Man", "Captain America", "New Superhero with a crazy long name"];
 var car_companies = ["Ford", "Chevrolet", "Toyota", "Honda", "Volkswagen", "Saab"];
 var birds = ["Pelican", "Toucan", "Ostrich", "Wren", "American Tree Sparrow"];
 
+var steve = {name: "Steven", age: 54};
+var tamir = {name: "Tamir", age: 54};
+
+objects_equal(steve, tamir);
 
 console.log(longest(superheroes));
 console.log(longest(car_companies));
